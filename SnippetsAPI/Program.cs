01ca5e8aa6f89815513f00bfd90e5f953a9dfc0b
Ikillnukes <ikillnukes1@gmail.com>
2018-01-08 00:07:39 +0100

    Initial changes

---
+++ SnippetsAPI/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MissingNumbersOnSequence
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<ulong> longs = (new ulong[] { 1, 2, 3, 6, 7, 8, 12, 13, 14, 17 }).ToList();
+
+            //Los dos metodos explicados
+            ulong numb = 0;
+            bool b = FindFirstMissingNumberFromSequence(longs, out numb); //Se devuelve una bool para saber si realmente había algún número que faltaba, puesto que por defecto se devolverá el 0, y el 0 puede formar parte de una secuencia a la cual no pertenece.
+
+            Console.WriteLine("First missing number: {0}\nMissing numbers: {1}", numb, string.Join(", ", FindMissingNumbersFromSequence(longs).Select(x => x.ToString()))); //Para comprobar si la secuencia estaba incompleta solo habría que comprobar x.Count() > 0...
+            Console.Read();
+        }
+
+        public static bool FindFirstMissingNumberFromSequence<T>(IEnumerable<T> arr, out T n)
+        {
+            if (!arr.Any(x => x.IsNumericType()))
+            {
+                Console.WriteLine("Type '{0}' can't be used as a numeric type!", typeof(T).Name);
+                n = default(T);
+                return false;
+            }
+
+            IOrderedEnumerable<T> list = arr.OrderBy(x => x);
+            bool b = false;
+            n = default(T);
+
+            foreach (T num in list)
+            {
+                b = (dynamic)num - n > 1;
+                if (b) break;
+                else n = (dynamic)num;
+            }
+
+            n = (dynamic)n + 1;
+
+            return b;
+        }
+
+        public static IEnumerable<T> FindMissingNumbersFromSequence<T>(IEnumerable<T> arr) where T : struct
+        {
+            if (!arr.Any(x => x.IsNumericType()))
+            {
+                Console.WriteLine("Type '{0}' can't be used as a numeric type!", typeof(T).Name);
+                yield break;
+            }
+
+            IOrderedEnumerable<T> list = arr.OrderBy(x => x);
+            T n = default(T);
+
+            foreach (T num in list)
+            {
+                T op = (dynamic)num - n;
+                if ((dynamic)op > 1)
+                {
+                    int max = op.ConvertValue<int>();
+                    for (int l = 1; l < max; ++l)
+                        yield return (dynamic)n + l.ConvertValue<T>();
+                }
+                n = (dynamic)num;
+            }
+        }
+
+    }
+
+    public static class Extensions
+    {
+        public static T ConvertValue<T>(this object o) where T : struct
+        {
+            return (T)Convert.ChangeType(o, typeof(T));
+        }
+
+        public static bool IsNumericType<T>(this T o)
+        {
+            return typeof(T).IsNumericType();
+        }
+
+        public static bool IsNumericType(this Type type)
+        {
+            switch (Type.GetTypeCode(type))
+            {
+                case TypeCode.Byte:
+                case TypeCode.SByte:
+                case TypeCode.UInt16:
+                case TypeCode.UInt32:
+                case TypeCode.UInt64:
+                case TypeCode.Int16:
+                case TypeCode.Int32:
+                case TypeCode.Int64:
+                case TypeCode.Decimal:
+                case TypeCode.Double:
+                case TypeCode.Single:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
