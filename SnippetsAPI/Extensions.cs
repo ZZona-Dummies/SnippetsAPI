e727fd2af8220f32e91758f03c7c3ef2f9af2ab1
Ikillnukes <ikillnukes1@gmail.com>
2018-01-08 00:10:17 +0100

    .

---
+++ SnippetsAPI/Extensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnippetsAPI
+{
+    public static class Extensions
+    {
+        public static T ConvertValue<T>(this object o) where T : struct
+        {
+            return (T)Convert.ChangeType(o, typeof(T));
+        }
+
+        public static bool IsNumericType<T>(this T o)
+        {
+            return typeof(T).IsNumericType();
+        }
+
+        public static bool IsNumericType(this Type type)
+        {
+            switch (Type.GetTypeCode(type))
+            {
+                case TypeCode.Byte:
+                case TypeCode.SByte:
+                case TypeCode.UInt16:
+                case TypeCode.UInt32:
+                case TypeCode.UInt64:
+                case TypeCode.Int16:
+                case TypeCode.Int32:
+                case TypeCode.Int64:
+                case TypeCode.Decimal:
+                case TypeCode.Double:
+                case TypeCode.Single:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
--- SnippetsAPI/Program.cs
+++ SnippetsAPI/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace MissingNumbersOnSequence
+namespace SnippetsAPI
 {
     public class Program
     {
@@ -67,39 +67,5 @@ namespace MissingNumbersOnSequence
             }
         }
 
-    }
-
-    public static class Extensions
-    {
-        public static T ConvertValue<T>(this object o) where T : struct
-        {
-            return (T)Convert.ChangeType(o, typeof(T));
-        }
-
-        public static bool IsNumericType<T>(this T o)
-        {
-            return typeof(T).IsNumericType();
-        }
-
-        public static bool IsNumericType(this Type type)
-        {
-            switch (Type.GetTypeCode(type))
-            {
-                case TypeCode.Byte:
-                case TypeCode.SByte:
-                case TypeCode.UInt16:
-                case TypeCode.UInt32:
-                case TypeCode.UInt64:
-                case TypeCode.Int16:
-                case TypeCode.Int32:
-                case TypeCode.Int64:
-                case TypeCode.Decimal:
-                case TypeCode.Double:
-                case TypeCode.Single:
-                    return true;
-                default:
-                    return false;
-            }
-        }
     }
 }
